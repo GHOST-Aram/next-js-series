6a537e18b781d7c925041f89fd299b4adf9990d8
GHOST-Aram <owinoojala@gmail.com>
2024-02-14 17:56:55 +0300

    chore: Implement nested routes

---
+++ src/app/blog/cultural/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Cultural = () => {
+  return (
+    <h1>Cultural Blog</h1>
+  )
+}
+
+export default Cultural
---
+++ src/app/blog/technical/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Teachnical = () => {
+  return (
+    <h1>Teachnical Blog</h1>
+  )
+}
+
+export default Teachnical
