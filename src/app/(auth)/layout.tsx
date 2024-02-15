977bca55ba175bf0b98a84312dff5d7ca05bd920
GHOST-Aram <owinoojala@gmail.com>
2024-02-15 17:27:02 +0300

    feat: Style active Links

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,21 +1,35 @@
 "use client"
 import { useState } from "react"
 import Link from "next/link"
+import { usePathname } from "next/navigation"
+import './style.css'
 
 export const AuthLayout = ({ children }: LayoutProps) =>{
-
+    const pathName = usePathname()
     const [input, setInput] = useState('')
+
+    
     return (
         <div style={{
             marginBlock: '.5rem'
         }}>
             <ul>
                 {
-                    links.map((link, index) =>(
-                        <li key={index}>
-                            <Link href={`/${link.pathName}`}>{link.displayName}</Link>
-                        </li>
-                    ))
+                    links.map((link, index) =>{
+
+                        const isActive = new RegExp(link.pathName).test(pathName)
+
+                        return (
+                            <li key={index}>
+                                <Link href={`/${link.pathName}`}
+                                    className={`font-bold ${isActive ? 'text-blue-700' : 'text-slate-800'}`}
+                                >
+                                    {link.displayName}
+                                </Link>
+                            </li>
+                        )
+                    })
+                        
                 }
             </ul>
             <input type="text" value={input} onChange={(e) => setInput(e.target.value)} />
