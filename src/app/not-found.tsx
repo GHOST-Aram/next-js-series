e33d13e1732dd12457f2c9b89e94e463aec2f46f
GHOST-Aram <owinoojala@gmail.com>
2024-02-15 09:14:24 +0300

    feat: Create custom 404 Page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <h1 style={{
+        color: '#333ddd',
+        marginBlock: '45vh',
+        margin: 'auto',
+    }}>Page Not Found</h1>
+  )
+}
+
+export default NotFound
