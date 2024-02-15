03024bd4162144bd432195d1ba5abd0c2c1f9a84
GHOST-Aram <owinoojala@gmail.com>
2024-02-15 19:33:13 +0300

    feat: Recover from errors
          - Create a reset button in the ErrorBoundary component
          - Handle the click event using the reset function provided
            in the ErrorBoundary props

--- src/app/services/error.tsx
+++ src/app/services/error.tsx
@@ -1,8 +1,15 @@
 "use client"
 import './style.css'
-const ErrorBoundary = ({ error }:{ error: Error}) =>{
+const ErrorBoundary = ({ error, reset }:{ error: Error, reset: () => void}) =>{
     return(
-        <h1 className="text-red-700 text-center text-3xl">{error.message}</h1>
+        <>
+            <h1 className="text-red-700 text-center text-3xl">{error.message}</h1>
+            <button 
+                onClick={reset}
+            className="px-8 py-2 rounded-md text-slate-800 bg-slate-300 border-solid m-auto">
+                Try Again
+            </button>
+        </>
     )
 }
 
