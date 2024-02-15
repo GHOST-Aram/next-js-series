c87d362e647cbf80c14d0d8c87b044f50a19dbc9
GHOST-Aram <owinoojala@gmail.com>
2024-02-15 14:42:48 +0300

    feat: Create dynamic links
         - Link product to details page using Links with dynamic
           product Id's

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,16 +1,41 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Products = () => {
   return (
     <>
         <h1>Products</h1>
-        <ul>
-            <li>Product 1</li>
-            <li>Product 2</li>
-            <li>Product 3</li>
+        <ul style={{ display: 'flex', flexDirection: 'column',}}>
+            {
+                products.map(product =>(
+                    <Link key={product.id} href={`/products/${product.id}`}>
+                        {product.name}
+                    </Link>
+                ))
+            }
+            
         </ul>
     </>
   )
 }
+
+const products = [
+    {
+        id: '43ed34de34de43der4',
+        name: 'Desktop'
+    },
+    {
+        id: '43ed34de34de43de78',
+        name: 'Cooking Gas'
+    },
+    {
+        id: '43ed34de34de43der3',
+        name: 'Table'
+    },
+    {
+        id: '43ed34de34de43der2',
+        name: 'Bed'
+    },
+]
 
 export default Products
