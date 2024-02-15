adb0186abf3aabac614e48cf6883e9f9e63bca4a
GHOST-Aram <owinoojala@gmail.com>
2024-02-15 15:08:05 +0300

    feat: Navigate Programmatically

--- src/app/homes/[homeId]/page.tsx
+++ src/app/homes/[homeId]/page.tsx
@@ -1,12 +1,58 @@
-import { notFound } from 'next/navigation'
+"use client"
+import { notFound, useRouter } from 'next/navigation'
 
 const HomeDetails = ({ params }: HomeParams) => {
-
+    const navigator = useRouter()
     if(Number(params.homeId) && Number(params.homeId) > 1000){
         return notFound()
     }
     return (
-        <div>HomeDetails { params.homeId }</div>
+        <div>
+            <h1>HomeDetails { params.homeId } </h1>
+            <button 
+                style={{ 
+                    paddingInline: '1rem',
+                    paddingBlock: '.25rem',
+                    color: 'white',
+                    backgroundColor: '#333ddd',
+                }}
+
+                onClick={() => {
+                    navigator.push('/place-order')
+                }}
+            >
+                Buy Now
+            </button>
+            <button 
+                style={{ 
+                    paddingInline: '1rem',
+                    paddingBlock: '.25rem',
+                    color: 'white',
+                    backgroundColor: '#333ddd',
+                }}
+
+                onClick={() => {
+                    navigator.back()
+                }}
+            >
+                Go Back to Listings
+            </button>
+            <button 
+                style={{ 
+                    paddingInline: '1rem',
+                    paddingBlock: '.25rem',
+                    color: 'white',
+                    backgroundColor: '#333ddd',
+                }}
+
+                onClick={() => {
+                    navigator.replace('/')
+                }}
+            >
+                Finish
+            </button>
+
+        </div>
     )
 }
 
---
+++ src/app/place-order/page.tsx
@@ -0,0 +1,7 @@
+const PlaceOrder = () =>{
+    return (
+        <h1>Order Page</h1>
+    )
+}
+
+export default PlaceOrder
