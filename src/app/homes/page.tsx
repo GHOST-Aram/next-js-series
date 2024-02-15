e5e44f9faad6eeb1ba7bec4ff64c623e4a68cebb
GHOST-Aram <owinoojala@gmail.com>
2024-02-15 14:49:03 +0300

    feat: Replace Navigation History

--- src/app/homes/page.tsx
+++ src/app/homes/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link"
 export const metadata = {
     title: 'Homes',
     description: 'Listed Homes for Sale'
@@ -8,9 +9,15 @@ const Homes = () => {
     <div>
         <h1>Homes</h1>
         <ul>
-            <li>Home 1</li>
-            <li>Home 2</li>
-            <li>Home 3</li>
+            <li>
+                <Link href={'homes/1'} replace>Home 1</Link>
+            </li>
+            <li>
+                <Link href={'homes/2'} replace>Home 2</Link>
+            </li>
+            <li>
+                <Link href={'homes/3'} replace>Home 3</Link>
+            </li>
         </ul>
     </div>
   )
